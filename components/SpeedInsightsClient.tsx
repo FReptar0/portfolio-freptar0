4c0d24fccf982a9b7a52666f68a9cf4a42cbd9ae
FReptar0 <20213tn137@utez.edu.mx>
2025-11-18 01:12:55 -0600

    fix: ajustar la indentación en SpeedInsightsClient para mejorar la legibilidad

--- components/SpeedInsightsClient.tsx
+++ components/SpeedInsightsClient.tsx
@@ -4,10 +4,10 @@ import dynamic from "next/dynamic";
 
 // Load SpeedInsights only on the client (no SSR)
 const SpeedInsights = dynamic(
-  () => import("@vercel/speed-insights/next").then((mod) => mod.SpeedInsights),
-  { ssr: false }
+    () => import("@vercel/speed-insights/next").then((mod) => mod.SpeedInsights),
+    { ssr: false }
 );
 
 export default function SpeedInsightsClient() {
-  return <SpeedInsights />;
+    return <SpeedInsights />;
 }
